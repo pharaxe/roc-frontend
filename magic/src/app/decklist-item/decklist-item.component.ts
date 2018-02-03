672a389c57ff9cd97e64cce93eb8ec230c21538c
bsweedler <bsweedler@gmail.com>
2018-02-02 17:01:53 -0800

    it shows fireball

--- magic/src/app/decklist-item/decklist-item.component.ts
+++ magic/src/app/decklist-item/decklist-item.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
+
+import {Card} from '../card';
+import {CardService} from '../card.service';
+
 
 @Component({
-  selector: 'app-decklist-item',
-  templateUrl: './decklist-item.component.html',
-  styleUrls: ['./decklist-item.component.css']
+   selector: 'app-decklist-item',
+   templateUrl: './decklist-item.component.html',
+   styleUrls: ['./decklist-item.component.css']
 })
 export class DecklistItemComponent implements OnInit {
+   @Input() public card: Card;
+   @Input() public id: number;
 
-  constructor() { }
+   constructor(  
+		private CardService: CardService,
+   )
+   {}
 
-  ngOnInit() {
-  }
+
+   ngOnInit() {
+		if (this.id) {
+         this.CardService.getCard(this.id).subscribe(card => this.card = card);
+		}
+   }
 
 }

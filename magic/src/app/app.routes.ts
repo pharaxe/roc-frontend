cebe11df10dc6d37f90472b358cdb43a2fb19e83
bsweedler <bsweedler@gmail.com>
2018-03-14 12:05:37 +0000

    less grid, more flexbox

--- magic/src/app/app.routes.ts
+++ magic/src/app/app.routes.ts
@@ -4,5 +4,6 @@ import {ArenaGridComponent} from './arena-grid/arena-grid.component';
 
 export const AppRoutes: Routes = [
    { path: '', component: ArenaGridComponent, pathMatch: 'full' },
-   { path: 'draft/:id', component: ArenaGridComponent }
+   { path: 'draft/:id', component: ArenaGridComponent },
+   { path: '*', component: ArenaGridComponent, pathMatch: 'full' },
 ];
